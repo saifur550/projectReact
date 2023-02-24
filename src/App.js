d63ef6773854b9937355a9db8cfe02d56c1dcc77
saifur550 <srstap550@gmail.com>
2023-02-25 05:49:19 +0600

    bs5 setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Header from './Assets/Header/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+   <>
+   <Header></Header>
+   </>
+  );
+}
+
+export default App;
